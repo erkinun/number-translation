c491f343646532163149e9d8fca1e6533b21de77
Erkin <erkinun@gmail.com>
2019-02-16 14:26:57 +0000

    initial dev

--- src/numtoenglish/core.clj
+++ src/numtoenglish/core.clj
@@ -1,7 +1,76 @@
-(ns num-.core
+(ns numtoenglish.core
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (println args))
+
+(def hede 5)
+
+(def small-numbers
+  {1 "one",
+   2 "two",
+   3 "three",
+   4 "four",
+   5 "five",
+   6 "six",
+   7 "seven",
+   8 "eight",
+   9 "nine",
+   10 "ten",
+   11 "eleven",
+   12 "twelve",
+   13 "thirteen",
+   14 "fourteen",
+   15 "fifteen",
+   16 "sixteen",
+   17 "seventeen",
+   18 "eighteen",
+   19 "nineteen"})
+
+(def ten-products
+  {20 "twenty",
+   30 "thirty",
+   40 "forty",
+   50 "fifty",
+   60 "sixty",
+   70 "seventy",
+   80 "eighty",
+   90 "ninety"})
+
+(defn check-boundary
+  [n]
+  (and (> n 0) (<= n 1000)))
+
+;; might not be needed at all
+(defn num->array
+  [num]
+  (reverse (.split (str num) "")))
+
+(defn hundreds->english
+  [digit]
+  (if (= digit 0) ""
+      (str (get small-numbers digit) " hundred")))
+
+(defn below-hundred->english
+  [num]
+  (if (<= num 20)
+    (get small-numbers num)
+    (str
+     (tens->english (* 10 (int (/ num 10))))
+     " "
+     (get small-numbers (mod num 10)))))
+
+(defn tens->english
+  [digit]
+  (if (= digit 0) ""
+      (str (get ten-products digit))))
+
+(defn number->english
+  [number]
+  (str
+   (hundreds->english (int (/ number 100)))
+   " and "
+   (below-hundred->english (mod number 100))))
