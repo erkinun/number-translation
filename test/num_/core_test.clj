dd455798ee20b8ddc463c6c70552f2b586c58cb9
Erkin <erkinun@gmail.com>
2019-02-16 14:53:32 +0000

    some testing

--- test/num_/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns num-.core-test
-  (:require [clojure.test :refer :all]
-            [num-.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
---
+++ test/numtoenglish/core_test.clj
@@ -0,0 +1,29 @@
+(ns numtoenglish.core-test
+  (:require [clojure.test :refer :all]
+            [numtoenglish.core :refer :all]))
+
+(deftest number->english->tests
+  (testing "1 should return one"
+    (is (= "one" (number->english 1))))
+  (testing "241 should return two hundred and forty one"
+    (is (= "two hundred and forty one" (number->english 241)))))
+
+(deftest only-tens-conversion
+  (testing "42 should return forty two"
+    (is (= "forty two" (number->english 42)))))
+
+(deftest only-hundreds-conversion
+  (testing "200 should return two hundred"
+    (is (= "two hundred" (number->english 200)))))
+
+(deftest hundreds->englist-test
+  (testing "2 should return as two hundred"
+    (is (= "two hundred" (hundreds->english 2)))))
+
+(deftest tens->english-test
+  (testing "30 should return as thirty"
+    (is (= "thirty" (tens->english 30)))))
+
+(deftest below-hundred-test
+  (testing "74 should return seventy four"
+    (is (= "seventy four" (below-hundred->english 74)))))
